01aef0b362b9ffd35ceda9c59a2d73018e301d54
Cesar Aguirre <cesar.aguirre.work@gmail.com>
2026-05-11 14:24:45 -0700

    add vista start menu orb

--- portfolio-site/src/components/footer/Footer.jsx
+++ portfolio-site/src/components/footer/Footer.jsx
@@ -1,13 +1,17 @@
+import StartMenuIcon from './StartMenuIcon'
+
 function Footer(){
     return (
         <footer
-            className="fixed bottom-0 left-0 right-0 z-50 h-10 border-t border-black/40"
+            className="fixed bottom-0 left-0 right-0 z-50 h-10 border-t border-black/40 flex items-center"
             style={{
                 backgroundImage:
                     'linear-gradient(to bottom, rgba(255,255,255,0.4) 0%, rgba(255,255,255,0.08) 8%, rgba(255,255,255,0) 16%), linear-gradient(to bottom, rgb(86,89,95) 0%, rgb(57,60,66) 36%, rgb(27,30,36) 64%, rgb(8,11,15) 100%)',
                 boxShadow: 'inset 0 1px 0 rgba(255,255,255,0.45), inset 0 -1px 0 rgba(0,0,0,0.8)',
             }}
-        ></footer>
+        >
+            <StartMenuIcon/>
+        </footer>
     )
 }
 
---
+++ portfolio-site/src/components/footer/StartMenuIcon.jsx
@@ -0,0 +1,7 @@
+import MenuIcon from '../../assets/vista-icons/vista-orb.png'
+function StartMenuIcon () {
+    return (
+        <img className="h-13 ml-3 cursor-pointer" src = { MenuIcon }></img>
+    )
+}
+export default StartMenuIcon
